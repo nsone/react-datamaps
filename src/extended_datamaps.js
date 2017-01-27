6357229216a2d6452cd4d40ad39fa978b5ffa97f
Jordan Byrd <jabyrd3@gmail.com>
2017-01-27 18:56:14 -0500

    change d3 import to try to get events

--- src/extended_datamaps.js
+++ src/extended_datamaps.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Datamaps from 'datamaps';
-import d3 from 'd3';
+import * as d3 from 'd3';
 
 export default class ExtendedDatamaps extends Datamaps {
 
